5775560bbbd57d6f7fb6b873f3c5ccbbc7345dca
Ryan Oates <ryanoatsie@outlook.com>
2025-06-13 05:27:47 -0700

    commit alters

--- wolfram_notebook_java_o3plan/src/main/java/com/example/notebook/config/SecurityFilterChain.java
+++ wolfram_notebook_java_o3plan/src/main/java/com/example/notebook/config/SecurityFilterChain.java
@@ -70,3 +70,5 @@ public class SecurityFilterChainConfig {
             );
 
         return http.build();
+    }
+}
--- wolfram_notebook_java_o3plan/src/main/java/com/example/notebook/config/SecurityFilterChainHttps.java
+++ wolfram_notebook_java_o3plan/src/main/java/com/example/notebook/config/SecurityFilterChainHttps.java
@@ -71,3 +71,5 @@ public class SecurityFilterChainHttpsConfig {
             );
 
         return http.build();
+    }
+}
